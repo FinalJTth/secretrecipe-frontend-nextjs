62cbca75c71eab452e740b76d5b2dc640640067e
FinalJTth <finaljtth_31@hotmail.com>
2023-11-07 06:17:51 +0700

    Install lint-staged and husky

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,18 +1,12 @@
-'use client'
+'use client';
 
-import { CacheProvider } from '@chakra-ui/next-js'
-import { ChakraProvider } from '@chakra-ui/react'
+import { CacheProvider } from '@chakra-ui/next-js';
+import { ChakraProvider } from '@chakra-ui/react';
 
-export function Providers({ 
-    children 
-  }: { 
-  children: React.ReactNode 
-  }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <CacheProvider>
-      <ChakraProvider>
-        {children}
-      </ChakraProvider>
+      <ChakraProvider>{children}</ChakraProvider>
     </CacheProvider>
-  )
+  );
 }
